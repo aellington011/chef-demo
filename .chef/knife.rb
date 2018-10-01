524e59a3303a43cf5356557ed297a31bcef6d44e
Aaron Ellington <aaron.ellington@venuenext.com>
2018-10-01 15:20:12 -0500

    changed chef server

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,10 +1,9 @@
-# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+#See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-ssl_verify_mode          :verify_none
-node_name                "admin"
-client_key               "#{current_dir}/admin.pem"
-chef_server_url          "https://52.40.115.57:4443/organizations/fuji"
+node_name                "aellington011"
+client_key               "#{current_dir}/aellington011.pem"
+chef_server_url          "https://api.chef.io/organizations/mt-fuji"
 cookbook_path            ["#{current_dir}/../cookbooks"]
--- cookbooks/java_se/attributes/bind.rb
+++
@@ -1,9 +0,0 @@
-# http://download.oracle.com/otn-pub/java/jdk/10.0.2+13/19aef61b38124481863b1413dce1855f/jdk-10.0.2_osx-x64_bin.dmg
-default['java_se']['release'] = '10.0.2'
-default['java_se']['build'] = '13'
-default['java_se']['hash'] = '19aef61b38124481863b1413dce1855f'
-
-# https://www.oracle.com/webfolder/s/digest/10-0-2checksum.html
-default['java_se']['sha256']['tar'] = '6633c20d53c50c20835364d0f3e172e0cbbce78fff81867488f22a6298fa372b'
-default['java_se']['sha256']['dmg'] = '2db323c9c93e7fb63e2ed7e06ce8150c32d782e3d0704be6274ebb2d298193aa'
-default['java_se']['sha256']['exe'] = 'bd2aa173db14789ac0369ab32bf929679760cae9e04d751d5f914ac3ad36c129'
